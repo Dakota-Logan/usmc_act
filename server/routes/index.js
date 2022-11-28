897783cca829c1032c2a1eccf6e0dbfebbb49b8b
Dakota-Logan <13681389+Dakota-Logan@users.noreply.github.com>
2022-11-28 15:19:19 -0500

    Added a couple more files.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router  = express.Router();
+
+let path = require("path");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.sendFile("../views/html/home.html");
 });
 
 module.exports = router;
