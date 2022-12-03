5a69cacfbf8049a3359a6e74a224d9ac622f446b
Dakota <xeliusone@gmail.com>
2022-12-03 11:21:44 -0500

    Almost finished with db script... finally, damn.

--- server/util/db.js
+++ server/util/db.js
@@ -1,47 +1,49 @@
 //* Database interface library. EZ BreZ
 const pgp = require('pg-promise')(/* options */),
 	  //* Database connection received from an environment variable.
-	  cn = pgp(process.env.database);
+	  cn  = pgp(process.env.database);
 
 //* Using a class to easily export into other file for use, good practice with classes.
 class deltaBravo {
+	constructor() {
+		//* Connect to the database with the interface.
+		private this.db = pgp(cn);
+	}
 	
-	//* Connect to the database with the interface.
-	db = pgp(cn);
-	  //? This is a parameterized query, which runs the variables as parameters so any hidden injection wont be run as postgres code. Neat lil feature I just found.
-	pq = require('pg-promise').ParameterizedQuery;
+	//? This is a parameterized query, which runs the variables as parameters so any hidden injection wont be run as postgres code. Neat lil feature I just found.
+	#pq = require('pg-promise').ParameterizedQuery;
 	
-	async login (lst, pwd) {
+	
+	async login( lst, pwd ) {
 		try {
-			let req = pq({text: "SELECT * FROM user_tracker WHERE first = $1 AND pwd = $2", values: [lst, pwd]});
+			let req = pq({ text: "SELECT * FROM user_tracker WHERE first = $1 AND pwd = $2", values: [ lst, pwd ] });
 			return await db.one(req);
 		} catch (e) {
 			throw 110;
 		}
 	}
 	
-	/* ! these functions need not authenticate the user, the auth script should be doing that, so all they should be doing is performing the database
-	!*	 operations, ie. check-in the user, return the roster, etc. */
-	async check_in (id) {
+	async check_in( id ) {
 		try {
-			let req = pg();//todo insert query
+			let req = new this.#pq({ text: "UPDATE user_tracker SET in = true WHERE id = $1", values: [id] });
+			return await this.db.one(req);
 		} catch (e) {
 			throw 111;
 		}
 	}
 	
-	check_out (id, reason) {
+	async check_out( id, reason ) {
 		try {
-			let req = pg();//todo insert query
+			let req = this.#pq({ text: "UPDATE user_tracker " });//todo insert query
+			return await this.db.one
 		} catch (e) {
 			throw 112;
 		}
 	}
 	
-	async roster (id) {
+	async roster() {
 		try {
-			let req = pg();//todo insert query
-			
+			return await this.db.one("SELECT * FROM user_tracker");
 		} catch (e) {
 			throw 113;
 		}
