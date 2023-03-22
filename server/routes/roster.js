40f496121d27eb33e53b68c58ff1d3c8e6256a74
XeliusOne@gmail.com <PbHvLu26C3pgwk7>
2023-03-22 07:12:29 -0400

    commit often

--- server/routes/roster.js
+++ server/routes/roster.js
@@ -1,11 +1,11 @@
-const express = require('express'),
-	  router  = express.Router(),
-	  db      = require("./../util/db");
+const express = require("express"),
+	router = express.Router(),
+	db = require("./../util/db");
 
 let path = require("path"),
 	htmlPath = __dirname + "/../views/html";
 
-router.get('/', async ( req, res, next ) => {
+router.get("/", async (req, res, next) => {
 	let users = (await db.roster()).rows;
 	console.log(users[0])
 	res.sendFile(path.join(htmlPath + "/roster.html"));
@@ -14,7 +14,7 @@ router.get('/', async ( req, res, next ) => {
 
 router.post("/update", async (req, res) => {
 	let [f, l] = req.body.name.split(".");
-	if(!req.body.password) throw 491;
+	if (!req.body.password) throw 491;
 	let hashNshit = auth.hash(req.body.password);
 	try {
 		console.log(db.changePassword(req.body.name, hashNshit));
